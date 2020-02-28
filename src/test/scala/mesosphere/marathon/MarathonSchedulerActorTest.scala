3ea873dd666cb665f2819b65629f77413e25b1a3
Tim Harper <tharper@mesosphere.com>
2020-02-27 20:11:03 -0700

    MarathonSchedulerActorTest passes

--- src/test/scala/mesosphere/marathon/MarathonSchedulerActorTest.scala
+++ src/test/scala/mesosphere/marathon/MarathonSchedulerActorTest.scala
@@ -3,8 +3,8 @@ package mesosphere.marathon
 import akka.Done
 import akka.actor.Props
 import akka.event.EventStream
-import akka.stream.{ActorMaterializer, OverflowStrategy}
 import akka.stream.scaladsl.{Keep, Source}
+import akka.stream.{ActorMaterializer, OverflowStrategy}
 import akka.testkit._
 import mesosphere.AkkaUnitTest
 import mesosphere.marathon.MarathonSchedulerActor._
@@ -23,7 +23,6 @@ import mesosphere.marathon.metrics.Metrics
 import mesosphere.marathon.metrics.dummy.DummyMetrics
 import mesosphere.marathon.state._
 import mesosphere.marathon.storage.repository.{DeploymentRepository, FrameworkIdRepository, GroupRepository, TaskFailureRepository}
-import scala.jdk.CollectionConverters._
 import mesosphere.marathon.stream.Subject
 import mesosphere.marathon.test.GroupCreation
 import org.apache.mesos.Protos.{Status, TaskStatus}
@@ -34,6 +33,7 @@ import org.scalatest.concurrent.Eventually
 import scala.collection.immutable.Set
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.jdk.CollectionConverters._
 
 class MarathonSchedulerActorTest extends AkkaUnitTest with ImplicitSender with GroupCreation with Eventually {
 
@@ -55,6 +55,7 @@ class MarathonSchedulerActorTest extends AkkaUnitTest with ImplicitSender with G
       leadershipTransitionInput.offer(LeadershipTransition.ElectedAsLeaderAndReady)
       eventually {
         verify(hcManager).reconcile(Seq(app))
+        ()
       }
       verify(deploymentRepo, times(1)).all()
     }
@@ -74,6 +75,7 @@ class MarathonSchedulerActorTest extends AkkaUnitTest with ImplicitSender with G
 
       eventually {
         verify(instanceTracker).setGoal(orphanedInstance.instanceId, Goal.Decommissioned, GoalChangeReason.Orphaned)
+        ()
       }
     }
 
@@ -199,6 +201,7 @@ class MarathonSchedulerActorTest extends AkkaUnitTest with ImplicitSender with G
 
       eventually {
         verify(queue).add(app, 1)
+        ()
       }
     }
 
@@ -213,6 +216,7 @@ class MarathonSchedulerActorTest extends AkkaUnitTest with ImplicitSender with G
 
       eventually {
         verify(queue).add(app, 1)
+        ()
       }
 
       expectMsg(RunSpecScaled(app.id))
@@ -490,4 +494,5 @@ class MarathonSchedulerActorTest extends AkkaUnitTest with ImplicitSender with G
       expectTerminated(deploymentManagerActor)
     }
   }
+
 }
